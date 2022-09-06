fab7b890c3ecb22fa67a12df35bcc37cf4e9d585
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-09-06 13:22:23 -0400

    Create domain-users.py

---
+++ ad/bloodhound-analysis-scripts/domain-users.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# 2021 Douglas Berdeaux
+# This script reads BloodHound-Collector users.json file and prints list of users.
+import json # for readinf JSON objects
+import sys # for exit and argv
+import os.path as path # for checking if file exists
+import codecs # for Windows-specific BloodHound Collector's JSON files.
+def usage(error):
+    print(f"[!] Error: {error}")
+    sys.exit(1)
+
+if len(sys.argv)!=2:
+    usage("Not enough arguments.")
+else:
+    users_file = sys.argv[1]
+    if path.exists(users_file):
+        with open(users_file) as file_contents:
+            json_users = file_contents.read()
+            decoded_users=codecs.decode(json_users.encode(), 'utf-8-sig')
+            json_dict = json.loads(decoded_users)
+            for obj in json_dict['users']:
+                print(obj['Properties']['name'])
+    else:
+        usage(f"Could not find file {sys.argv[1]}")
